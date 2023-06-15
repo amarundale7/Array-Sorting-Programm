0ab3305ba60e4af7359923cee82580437032b02e
amardeep <amarundale7@gmail.com>
2023-06-15 15:28:32 +0530

    code correction using another array

--- src/com/array/sorting/SumOfTwoNumber.java
+++ src/com/array/sorting/SumOfTwoNumber.java
@@ -4,6 +4,7 @@ public class SumOfTwoNumber {
 	public static void main(String[] args) {
 		
 		int a[]= {1,2,3,4,5,6,7,8,9};
+		int b [] = {2,4,3,5,1,6,8,7,9};
 		int sum = 6;
 		int count = 0;
 		for (int i = 0; i < a.length; i++) {
@@ -11,12 +12,14 @@ public class SumOfTwoNumber {
 				if (a[j]==sum-a[i]) {
 					
 					System.out.println(a[j]+ "+" +a[i]);
+					System.out.println(b[i]+ "+" +b[i]);
 					
 					count++;
 				}
 				
 			}
 			System.out.print(count);
+			System.out.println(count);
 		}
 	}
 
