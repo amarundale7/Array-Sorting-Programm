cf6cab661467376e8b668da03c1b41bc1fcecc9e
amardeep <amarundale7@gmail.com>
2023-06-28 16:15:14 +0530

    HashMap added

---
+++ src/com/array/sorting/HashMap.java
@@ -0,0 +1,17 @@
+package com.array.sorting;
+
+import java.util.ArrayList;
+
+public class HashMap {
+	
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add(2);
+		list.add(5);
+		list.add(3);
+		list.stream().forEach(x-> System.out.println(x));
+		
+		
+	}
+
+}
--- src/com/array/sorting/SumOfTwoNumber.java
+++ src/com/array/sorting/SumOfTwoNumber.java
@@ -6,7 +6,7 @@ public class SumOfTwoNumber {
 		int a[]= {1,2,3,4,5,6,7,8,9};
 		int b [] = {2,4,3,5,1,6,8,7,9};
 		int sum = 6;
-		int count = 0;
+		int add = 0;
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 0; j < a.length; j++) {
 				if (a[j]==sum-a[i]) {
@@ -14,12 +14,12 @@ public class SumOfTwoNumber {
 					System.out.println(a[j]+ "+" +a[i]);
 					System.out.println(b[i]+ "+" +b[i]);
 					
-					count++;
+					add++;
 				}
 				
 			}
-			System.out.print(count);
-			System.out.println(count);
+			System.out.print(add);
+			System.out.println(add);
 		}
 	}
 
